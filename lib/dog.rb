dc81f7a12cec5687e6c3b1602478a2a80a1de039
Arthur Song <arth3rs0ng@gmail.com>
2019-04-22 00:48:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,4 +80,14 @@ class Dog
       self.new_from_db(row)
     end.first
   end
+  
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+    
+    DB[:conn].execute(sql, @name, @breed, @id)
+  end
 end
